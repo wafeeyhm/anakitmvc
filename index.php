185e38c2a0949b34b9dd67bb25bb9520c4f0c810
wafiy@anak.it <wafiy@anak.it>
2024-11-04 16:34:53 +0800

    initial setup

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-133853298-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-133853298-1');
+</script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- PAGE settings -->
+  <link rel="icon" href="favicon.ico">
+  <title>Anak IT Brunei</title>
+  <!-- CSS dependencies -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" type="text/css">
+  <link rel="stylesheet" href="elegant.css" type="text/css">
+  <meta name="description" content="Anak IT Brunei">
+  <meta name="keywords" content="anak it, anak, it, brunei, bn, anakitbn">
+<script defer src="https://www.webforce.digital/analytics/pixel/jmNXHqFHk6noqVjP"></script>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md bg-primary navbar-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#"><i class="fa d-inline fa-lg fa-power-off"></i><b>&nbsp;ANAK IT BN</b></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#about"><i class="fas fa-info-circle fa-fw"></i> ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#portfolio"><i class="far fa-folder-open fa-fw"></i> PORTFOLIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="images/" target="_blank"><i class="far fa-images fa-fw"></i><i></i> PHOTOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://tools.anak.it/" target="_blank"><i class="fas fa-wrench fa-fw"></i> TOOLS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://blog.anak.it/" target="_blank"><i class="fab fa-blogger fa-fw"></i> BLOG</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://play.google.com/store/apps/details?id=appinventor.ai_app.AmalBN" target="_blank"><i class="fab fa-meetup fa-fw"></i><i></i> AMAL BN APP</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="py-5 text-center" style="background-image: url('assets/cosmo.jpg');background-position:center center;background-repeat:no-repeat;">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="display-3 mb-4 text-center text-white bg-dark-opaque"><br><img class="img-fluid d-block mx-auto" src="assets/logo.png" width="40%">
+          <p> </p>ANAK IT BN<br><br></h1>
+          <p class="lead mb-5">WELCOME TO ANAK IT BN</p>
+          <a href="https://www.meetup.com/Anak-IT-Brunei/" class="btn btn-lg btn-primary mx-1" target="_blank">Go to our Meetup Page</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 text-center bg-secondary" id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-12">
+              <h1 class="text-light">ABOUT</h1>
+              <img class="d-block mx-auto img-fluid" src="assets/about-anak-it.jpg">
+              <p class="text-light">
+                <br>ANAK IT is an open, non-profit community created as a centre of learning for the locals to share their knowledge in the IT field. Established in 2010 at Institut Teknologi Brunei (now UTB) by a group of like-minded inviduals who like to explore and spread their passion. Anak IT Brunei was soft-launched by Ministry of Culture Youth and Sports in 2011 and has continued to work closely with various agencies to increase awareness on various aspects in ICT as well as conduct workshops, seminars etc.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5" id="portfolio">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center">PORTFOLIO</h1>
+          <p class="lead text-center">We do what we love and we make sure you do too. </p>
+        </div>
+      </div>
+      <div class="row mb-5">
+        <div class="col-md-7">
+          <h2 class="text-light">Workshops, Seminar, Crash Courses</h2>
+          <p class="">We like to spread the love for networking, information security, programming languages and multimedia development. If you would like to learn something else, do let us know! </p>
+        </div>
+        <div class="col-md-5 align-self-center">
+          <img class="img-fluid d-block w-100 img-thumbnail" src="assets/class1.jpg"> </div>
+      </div>
+      <div class="row">
+        <div class="col-md-5">
+          <img class="img-fluid d-block mb-4 w-100 img-thumbnail" src="assets/drop1.jpg"> </div>
+        <div class="col-md-7">
+          <h2 class="pt-3 text-light">Drop-in / Sharing Sessions</h2>
+          <p class=""> Open sharing sessions for everyone to talk or discuss (really) about anything! We also make use of these sessions to get to know one another. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 text-white bg-secondary">
+    <div class="container">
+      <div class="row">
+        <div class="align-self-center p-5 col-md-6">
+          <h1 class="mb-4">Hour of Code</h1>
+          <p class="mb-5 text-justify">The Hour of Code is a global movement reaching tens of millions of students in 180+ countries and we have successfully hosted the annual session for two consecutive years.</p>
+          <h1 class="mb-4">Waspada Siber</h1>
+          <p class="mb-5 text-justify">Waspada Siber is a weekly radio programme for Nasional FM Radio Station where the contents are provided by Anak IT BN as an additional medium in spreading Cyber Security awareness to public.</p>
+          <h1 class="mb-4">Affiliations</h1>
+          <p class="mb-5 text-justify">We actively seek to establish collaborations with industry players, government agencies and other like-minded communities.</p>
+        </div>
+        <div class="col-md-6 p-0">
+          <div class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img src="assets/hoc1.jpg" atl="first slide" class="d-block img-fluid w-100 img-thumbnail"> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center">
+            <br>STEERING MEMBERS</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3 align-self-center text-center p-4 col-6"> <i style="background-image: url('assets/zul1.jpg');"></i>
+          <img class="img-fluid d-block rounded-circle mx-auto" src="assets/zul1.jpg" width="70%">
+          <h3 class="my-2">ZULFADLY</h3>
+        </div>
+        <div class="col-md-3 align-self-center text-center p-4 col-6">
+          <img class="img-fluid d-block mx-auto rounded-circle" src="assets/wafiy1.jpg" width="70%">
+          <h3 class="my-2">WAFIY</h3>
+        </div>
+        <div class="col-md-3 align-self-center text-center p-4 col-6">
+          <img class="d-block mx-auto rounded-circle img-fluid" src="assets/qoyyum1.jpg" width="70%">
+          <h3 class="my-2">QOYYUUM</h3>
+        </div>
+        <div class="col-md-3 align-self-center text-center p-4 col-6">
+          <img class="img-fluid d-block mx-auto rounded-circle" src="assets/farhan1.jpg" width="70%">
+          <h3 class="my-2">FARHAN</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 align-self-center text-center p-4 col-6">
+          <img class="img-fluid d-block mx-auto rounded-circle" src="assets/fadhil1.jpg" width="70%">
+          <h3 class="my-2">FADHIL</h3>
+        </div>
+        <div class="col-md-3 align-self-center text-center p-4 col-6">
+          <img class="img-fluid d-block mx-auto rounded-circle" src="assets/fitri1.jpg" width="70%">
+          <h3 class="my-2">FITRI</h3>
+        </div>
+        <div class="col-md-3 align-self-center text-center p-4 col-6">
+          <img class="img-fluid d-block mx-auto rounded-circle" src="assets/izdi.jpg" width="70%">
+          <h3 class="my-2">IZDI</h3>
+        </div>
+        <div class="col-md-3 align-self-center text-center p-4 col-6">
+          <img class="img-fluid d-block mx-auto rounded-circle" src="assets/lee.jpg" width="70%">
+          <h3 class="my-2">MOHD LEE</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-dark text-white">
+    <div class="container">
+      <div class="row text-right">
+        <div class="col-md-8">
+          <p class="text-left">CONTACT
+            <img src="assets/mail.png"> </p>
+        </div>
+        <div class="col-md-1 align-self-center col-3">
+          <a href="https://www.facebook.com/anakitbrunei" target="_blank"><i class="fab fa-fw fa-facebook fa-3x text-white"></i></a>
+        </div>
+        <div class="col-md-1 align-self-center col-3">
+          <a href="https://twitter.com/anakitbrunei" target="_blank"><i class="fab fa-fw fa-twitter fa-3x text-white"></i></a>
+        </div>
+        <div class="col-md-1 align-self-center col-3">
+          <a href="https://www.instagram.com/anakitbn/" target="_blank"><i class="fab fa-fw fa-instagram text-white fa-3x"></i></a>
+        </div>
+        <div class="col-md-1 align-self-center col-3">
+          <a href="https://ifb.org.bn/InterestGroup.html" target="_blank"><i class="fab fa-fw fa-meetup text-white fa-3x"></i></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 mt-3 text-center">
+          <p>All Rights Reserved.<br>Copyright © 2010-2024 Anak IT BN</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="jquery-3.2.1.js"></script>
+  <script src="popper.min.js"></script>
+  <script src="bootstrap.min.js"></script>
+</body>
+
+</html>
